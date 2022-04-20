9199ddbc1fec529ac4f0c9ebbc85bc85e6cc3385
Jiacong Wu <jwu102@sheffield.ac.uk>
2022-04-20 15:53:10 +0100

    using single node for task2, with naive Levy flight implementation

--- src/tb3.py
+++ src/tb3.py
@@ -5,7 +5,7 @@ from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import LaserScan
 from tf.transformations import euler_from_quaternion
-from math import degrees
+from math import degrees, sqrt
 import numpy as np
 
 class Tb3Move(object):
@@ -35,28 +35,73 @@ class Tb3Odometry(object):
         self.yaw = self.round(degrees(yaw), 4)
         self.posx = self.round(position.x, 4)
         self.posy = self.round(position.y, 4)
+
+        # calculate distance travelled for Levy flights step size
+        if self.startup:
+            self.previous_x = self.posx
+            self.previous_y = self.posy
+        self.startup = False
+
+        d_increment = sqrt((self.posx - self.previous_x) ** 2 + (self.posy - self.previous_y) ** 2)
+        self.total_distance = self.total_distance + d_increment
+
+        self.previous_x = self.posx
+        self.previous_y = self.posy
     
     def __init__(self):
         self.posx = 0.0
         self.posy = 0.0
         self.yaw = 0.0
         self.subscriber = rospy.Subscriber('/odom', Odometry, self.odom_cb)
-    
+        
+        self.total_distance = 0.0
+        self.previous_x = 0.0
+        self.previous_y = 0.0
+        self.startup = True
+
+
     def round(self, value, precision):
         value = int(value * (10**precision))
         return float(value) / (10**precision)
 
 class Tb3LaserScan(object):
     def laserscan_cb(self, scan_data: LaserScan):
-        left_arc = scan_data.ranges[0:31]
-        right_arc = scan_data.ranges[-30:]
-        front_arc = np.array(left_arc[::-1] + right_arc[::-1])
+
+        # front_arc = np.array(scan_data.ranges[0:21][::-1] + scan_data.ranges[-20:][::-1])
+        # self.min_distance = front_arc.min()
+        # arc_angles = np.arange(-20, 21)
+        # self.closest_object_position = arc_angles[np.argmin(front_arc)]
+
+        #============================
+        front_region = np.array((scan_data.ranges[-20:] + scan_data.ranges[0:21])[::-1])
+        left_region = np.array(scan_data.ranges[21:62][::-1])
+        right_region = np.array(scan_data.ranges[-40:340][::-1])
         
+        front_arc = np.concatenate([left_region, front_region, right_region])
         self.min_distance = front_arc.min()
-        arc_angles = np.arange(-20, 21)
+        arc_angles = np.arange(-60, 61)
         self.closest_object_position = arc_angles[np.argmin(front_arc)]
 
+        self.front_min_distance = front_region.min()
+        self.front_max_distance = front_region.max()
+
+        self.left_max_distance = left_region.max()
+        self.right_max_distance = right_region.max()
+        self.full_max_distance = np.array(scan_data.ranges).max()
+        
     def __init__(self):
         self.min_distance = 0.0
         self.closest_object_position = 0.0 # degrees
-        self.subscriber = rospy.Subscriber('/scan', LaserScan, self.laserscan_cb) 
+        self.subscriber = rospy.Subscriber('/scan', LaserScan, self.laserscan_cb)
+
+        #=============================
+        self.front_min_distance = 0.0
+        self.front_closest_object_angle = 0
+        self.front_max_distance = 0.0
+
+        self.full_max_distance = 0.0
+
+        self.left_max_distance = 0.0
+        
+        self.right_max_distance = 0.0
+        self.right_whisker_distance = 0.0
